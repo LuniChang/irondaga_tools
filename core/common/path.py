0675be60b2eece4130bd71bb2401736b001577f7
LuniChang <244410210@qq.com>
2020-05-28 00:26:47 +0800

    change 2 img hash

--- core/common/path.py
+++ core/common/path.py
@@ -1,7 +1,13 @@
 import os
+import sys
 
 def getProjectPath():
-    project_name = "irondaga_tools"
-    curPath = os.path.abspath(os.path.dirname(__file__))
-    rootPath = curPath[:curPath.find(project_name+"\\")+len(project_name+"\\")] 
-    return rootPath
+    if hasattr(sys, 'frozen'):
+        # Handles PyInstaller
+        return os.path.dirname(sys.executable)+"\\"  #使用pyinstaller打包后的exe目录
+    else :
+        project_name = "irondaga_tools"
+        curPath = os.path.abspath(os.path.dirname(__file__))
+        rootPath = curPath[:curPath.find(project_name+"\\")+len(project_name+"\\")] 
+        return rootPath
+
