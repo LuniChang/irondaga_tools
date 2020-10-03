0ab5e77a2e208049c0aa355b0cba1dc263bc5079
LuniChang <244410210@qq.com>
2020-10-03 17:20:05 +0800

    Update reply_wushuang.py

--- core/control/reply_wushuang.py
+++ core/control/reply_wushuang.py
@@ -66,7 +66,7 @@ class ReplyWuShuang(BaseControl):
                 print("onmap")
                 # if screen.autoCompareResImgHash(self.handle,"ws\\on_ws_map_40_70_60_74.png"):
      
-                if self.matchResImgInWindow("zhangjiepingjia_40_86_60_89.png")
+                if self.matchResImgInWindow("zhangjiepingjia_40_86_60_89.png"):
                     # self._MapNo=self._MapNo%6
                     if self._MapNo<3:
                        self.leftClickPer(20+35*self._MapNo,30)
