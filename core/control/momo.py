1acca0dcfc86737d65732632e2bf59e950bd1fc0
ling <244410210@qq.com>
2020-11-29 10:55:02 +0800

    fix bug

--- core/control/momo.py
+++ core/control/momo.py
@@ -28,18 +28,21 @@ class Momo(BaseControl):
     
 
     def run(self):    
-
+        self._count=0
         while self._isRun and self._count< self.maxCount:
             screen.setForegroundWindow(self.handle)
 
      
             if self.matchResImgInWindow("momo/home_0_0_30_5.png"):
-                self.leftClickPer(10, 50)
+                self.leftClickPer(8, 50)
                 time.sleep(3)
                 self.onDlgOkAndClick()
-                time.sleep(3)
+                time.sleep(4)
                 self.clickMacthImg("momo/next_88_70_98_88.png")
+                self.leftClickPer(95, 80)
+                
                 self._count=self._count+1
+                time.sleep(4)
 
   
 
